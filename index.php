87b0c8f58743b5080b9117555af4a4bd810153d1
Miguel Henrique <miguelhp373@gmail.com>
2021-10-25 19:02:54 -0300

    fix name favicon

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if (isset($_SESSION['Msg_error'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Poupa+ | Web</title>
-  <link rel="shortcut icon" href="favicon.svg" type="image/x-icon">
+  <link rel="shortcut icon" href="Favicon.svg" type="image/x-icon">
 
   <!--Criado em 06/08/2021-->
 
