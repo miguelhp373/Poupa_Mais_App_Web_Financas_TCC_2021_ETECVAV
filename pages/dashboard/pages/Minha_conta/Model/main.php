a39de45684a83ad9c42d59e2b26fd07272ac4a2c
Miguel Henrique <miguelhp373@gmail.com>
2021-11-09 22:06:04 -0300

    remove comment

--- pages/dashboard/pages/Minha_conta/Model/main.php
+++ pages/dashboard/pages/Minha_conta/Model/main.php
@@ -16,48 +16,10 @@ $_SESSION['Msg_error'] = '';
 
 $UserName                   =   filter_input(INPUT_POST,'nome',FILTER_SANITIZE_STRING);
 $userEmail                  =   filter_input(INPUT_POST,'email',FILTER_SANITIZE_EMAIL);
-//$UserCpf                    =   filter_input(INPUT_POST,'cpf',FILTER_SANITIZE_STRING);   
 $UserPhoneNumber            =   filter_input(INPUT_POST,'telefone',FILTER_SANITIZE_STRING);
 $UserPassVerify             =   filter_input(INPUT_POST,'senha',FILTER_SANITIZE_STRING);
 $UserPass                   =   password_hash(filter_input(INPUT_POST,'senha',FILTER_SANITIZE_STRING),PASSWORD_DEFAULT);            
 
-
-// function validaCPF($cpf) {
- 
-//     // Extrai somente os números
-//     $cpf = preg_replace( '/[^0-9]/is', '', $cpf );
-     
-//     // Verifica se foi informado todos os digitos corretamente
-//     if (strlen($cpf) != 11) {
-//         $_SESSION['Msg_error']  =   "CPF Inválido.";
-//         header('Location: ../index.php');
-//         die();
-//     }
-
-//     // Verifica se foi informada uma sequência de digitos repetidos. Ex: 111.111.111-11
-//     if (preg_match('/(\d)\1{10}/', $cpf)) {
-//         $_SESSION['Msg_error']  =   "CPF Inválido.";
-//         header('Location: ../index.php');
-//         die();
-//     }
-
-//     // Faz o calculo para validar o CPF
-//     for ($t = 9; $t < 11; $t++) {
-//         for ($d = 0, $c = 0; $c < $t; $c++) {
-//             $d += $cpf[$c] * (($t + 1) - $c);
-//         }
-//         $d = ((10 * $d) % 11) % 10;
-//         if ($cpf[$c] != $d) {
-//             $_SESSION['Msg_error']  =   "CPF Inválido.";
-//             header('Location: ../index.php');
-//             die();
-//         }
-//     }
-//     return true;
-
-// }
-
-// validaCPF($UserCpf);
 
 try {
 
