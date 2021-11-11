2855a5746e2f94d1bb6f74b99400e0df9a9a80e1
Miguel Henrique <miguelhp373@gmail.com>
2021-11-11 20:32:48 -0300

    alter favicon on application

--- pages/dashboard/pages/Painel_do_Administrador/index.php
+++ pages/dashboard/pages/Painel_do_Administrador/index.php
@@ -130,7 +130,7 @@ try {
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Painel do Administrador | Poupa+</title>
-    <link rel="shortcut icon" href="../../../../Favicon.svg" type="image/x-icon">
+    <link rel="shortcut icon" href="../../../../Favicon.png" type="image/x-icon">
     <!--Jquery-->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
--- pages/login/index.php
+++ pages/login/index.php
@@ -103,7 +103,7 @@ if (isset($_GET['page'])) {
   <meta http-equiv="X-UA-Compatible" content="IE=edge" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Login | Poupa+</title>
-  <link rel="shortcut icon" href="../../Favicon.svg" type="image/x-icon">
+  <link rel="shortcut icon" href="../../Favicon.png" type="image/x-icon">
   <!--Criado em 06/08/2021-->
 
   <!--Jquery-->
